51a3bb40b552592b085a0722d14d235b46b67975
chicacheca <kristyna.swaczynova@gmail.com>
2024-10-16 17:45:52 +0200

    HW - screenshot elements on Registration page

--- src/tests/homework.spec.js
+++ src/tests/homework.spec.js
@@ -6,3 +6,14 @@ test("should open registration page", async ({ page }) => {
     await page.screenshot({path: 'registration_page.png'});
 });
  
+test("should screenshot elements", async ({ page }) => {
+    await page.goto("/registrace");
+    console.log(await page.title());
+    
+    await page.screenshot({path: 'registration_page.png'});
+    await page.locator("#name").screenshot({path: "reg_jmeno.png"});
+    await page.locator("#email").screenshot({path: "reg_email.png"});
+    await page.locator("#password").screenshot({path: "reg_password.png"});
+    await page.locator("#password-confirm").screenshot({path: "reg_password-confirm.png"});
+    await page.locator(".btn-primary").screenshot({path: "reg_Zaregistrovat_btn.png"});
+});
