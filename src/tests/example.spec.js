b11025ec06c46e6ca1a607a209f50994c9c38aaa
chicacheca <kristyna.swaczynova@gmail.com>
2024-11-09 12:43:02 +0100

    Exercise - basic interaction with browser

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -3,4 +3,11 @@ import { test } from "@playwright/test";
 test("should open login page", async ({ page }) => {
     await page.goto("/prihlaseni");
     console.log(await page.title());
+    console.log("This is my first test!");
+
+    await page.setViewportSize({ width: 800, height: 600 });
+    await page.screenshot({ path: "login_page_800_600.png" });
+
+    await page.setViewportSize({ width: 1920, height: 1080 });
+    await page.screenshot({ path: "login_page_1920_1080.png" });
 });
