0d39c288b4d475779a3e4f9c38158aa959658220
chicacheca <kristyna.swaczynova@gmail.com>
2024-12-07 10:51:00 +0100

    Exercise - test organization, describe, hooks, tags, annotations

--- src/tests/example.spec.js
+++ src/tests/example.spec.js
@@ -1,195 +1,194 @@
-import { expect, test } from "@playwright/test";
-import { username, password, userFullName, applicationsSearchText } from "../fixtures/fixtures"
-import { RegExp } from "../fixtures/regular-expressions";
+import {expect, test} from "@playwright/test";
+import {
+    username,
+    password,
+    userFullName,
+    applicationsPageSize,
+    applicationsSearchText
+} from "../fixtures/fixtures.js"
+import {RegExp} from "../fixtures/regular-expressions.js";
 
-/* Cvičení 1:
-    1. Ověř stav přihlašovacího formuláře
-        a) Políčka a tlačítko pro příhlášení jsou viditelná
-        b) Tlačítko pro příhlášení obsahuje správný text
-    2. Přihlaš se do aplikace a ověř, že
-        a) formulář přihlášení není vidět.
-        b) je v pravém horním rohu správné jméno uživatele
-    3. Optional: Zkus si nějaké další asertace
-        a) Zkus si negaci .not
-        b) Vyzkoušej porovnání screenshotů
-*/
-test("Input fields and Submit button for Login are visible", async ({ page }) => {
-    await page.goto("/prihlaseni");
+/*
+Lekce 5: Organizace testů
 
-    const emailField = page.getByLabel("Email");
-    const passwordField = page.getByLabel("Heslo");
-    const loginButton = page.getByRole("button", { name: "Přihlásit" });
+1. Dlouhý test rozděl mezi jednotlivé testy a zorganizuj je pomocí hooks test.beforeAll, test.beforeEach, test.afterAll, test.afterEach.
+Měly by vzniknout pravděpodobně alespoň 4 samostatné testy:
+    a. test pro nesprávné přihlášení
+    b. test pro správné přihlášení
+    c. test pro odhlášení
+    d. test pro výpis tabulky přihlášek bez filtrování
+    e. test pro filtrování tabulky přihlášek
+Rozděl testy do dvou describe bloků:
+    - jedna sekce bude obsahovat testy pro login
+    - druhá sekce bude obsahovat testy pro tabulku přihlášek 
+Nezapomeň testy i obě sekce hezky popsat.
 
-    await expect(emailField, "email field should be visible").toBeVisible();
-    await expect(passwordField, "password field should be visible").toBeVisible();
-    await expect(loginButton, "login button should be visible").toBeVisible("Přihlásit");
-});
-
-test("Login button has correct text 'Přihlásit'", async ({ page }) => {
-    await page.goto("/prihlaseni");
-
-    const loginButton = page.getByRole("button", { name: "Přihlásit" });
-    await expect(loginButton, "login button should have correct text").toHaveText("Přihlásit");
-});
-
-test("Login form is not visible once logged in", async ({ page }) => {
-    await page.goto("/prihlaseni");
-
-    const emailField = page.getByLabel("Email");
-    const passwordField = page.getByLabel("Heslo");
-    const loginButton = page.getByRole("button", { name: "Přihlásit" });
-
-    await emailField.fill(username);
-    await passwordField.fill(password);
-    await loginButton.click();
-
-    await expect(emailField).not.toBeAttached();
-    await expect(passwordField).not.toBeAttached();
-    await expect(loginButton).not.toBeAttached("Přihlásit");
-});
-
-test("Correct user name should be displayed once logged in", async ({ page }) => {
-    await page.goto("/prihlaseni");
-
-    const emailField = page.getByLabel("Email");
-    const passwordField = page.getByLabel("Heslo");
-    const loginButton = page.getByRole("button", { name: "Přihlásit" });
-
-    await emailField.fill(username);
-    await passwordField.fill(password);
-    await loginButton.click();
-
-    const userNameLocator = page.locator(".navbar-right").locator("strong");
-    await expect(userNameLocator).toHaveText(userFullName);
-});
-
-test("Screenshots of the login page should match", async ({ page }) => {
-    await page.goto("/prihlaseni");
-    await expect(page).toHaveScreenshot("login_page.png");
-});
-    
-/* Cvičení 2:
-    Uprav test pro tabulku přihlášek tak, abys ověřila, že
-    1. Po přihlášení tabulka obsahuje správný počet přihlášek.
-    2. Každá přihláška obsahuje:
-        a) jméno účastníka
-        b) datum konání
-        c) typ platby
-        d) zbývající částku k doplacení
-    Tip: K asertaci textového obsahu podle nějakého vzoru můžete použít regulární výrazy.
+2. Zkus do testů doplnit další detaily:
+    a. Označ si nějaký test tagem
+    b. Přidej si k testu anotace
+    c. Použij test.step pro popis jednotlivého kroku testu
 */
 
-test("Applications table contains correct amount of applications", async ({ page }) => {
-    await page.goto("/prihlaseni");
+test.describe("Login Page", () => {
+    test.beforeEach( async({ page }) => { 
+        await page.goto("/prihlaseni");
+     });
 
-    const emailField = page.getByLabel("Email");
-    const passwordField = page.getByLabel("Heslo");
-    const loginButton = page.getByRole("button", { name: "Přihlásit" });
+     test("Should display login form", async({ page }) => {
+        const emailField = page.getByLabel('Email');
+        await expect(emailField, 'email field should be visible').toBeVisible();
+        await expect(emailField, 'email field should be enabled').toBeEnabled();
+
+        const passwordField = page.getByLabel('Heslo');
+        await expect(passwordField, 'password field should be visible').toBeVisible();
+        await expect(passwordField, 'password field should be enabled').toBeEnabled();
+
+        const loginButton = page.getByRole('button', { name: 'Přihlásit'});
+        await expect(loginButton, 'login button should be visible').toBeVisible();
+        await expect(loginButton, 'login button text should have text').toHaveText('Přihlásit');
+     });
+
+     test("Should log in with valid credentials", { tag: "@smoke" }, async ({ page }) => {
+        await page.getByLabel("Email").fill(username);
+        await page.getByLabel("Heslo").fill(password);
+        await page.getByRole("button", { name: "Přihlásit"}).click();
+
+        const currentUser = page
+            .locator(".navbar-right")
+            .locator("strong");
+        await expect(currentUser, "current user should be displayed").toHaveText(userFullName);
+    });
+
+    test("Should not log in with invalid password", async ({ page }) => {
+        await page.getByLabel("Email").fill(username);
+        await page.getByLabel("Heslo").fill("invalid");
+        await page.getByRole("button", { name: "Přihlásit"}).click();
+
+        const toastMessage = page.locator(".toast-message");
+        await expect(toastMessage).toHaveText("Některé pole obsahuje špatně zadanou hodnotu");
+        const fieldError = page.locator(".invalid-feedback");
+        await expect(fieldError).toHaveText("Tyto přihlašovací údaje neodpovídají žadnému záznamu.");
+
+        await expect(page.getByLabel("Email"), "email field should be visible").toBeVisible();
+        await expect(page.getByLabel("Heslo"), "password field should be visible").toBeVisible();
+        await expect(page.getByRole("button", { name: "Přihlásit"}), "login buton should be visible").toBeVisible();
+    });
+
+    test("Should not log in with invalid email", async ({ page }) => {
+        await page.getByLabel("Email").fill("abcgmail.com");
+        await page.getByLabel("Heslo").fill(password);
+        await page.getByRole("button", { name: "Přihlásit"}).click();
+
+        await expect(page.getByLabel("Email"), "email field should be visible").toBeVisible();
+        await expect(page.getByLabel("Heslo"), "password field should be visible").toBeVisible();
+        await expect(page.getByRole("button", { name: "Přihlásit"}), "login button should be visible").toBeVisible();
+    });
+
+    test("Should log out", async ({ page }) => {
+        await page.getByLabel("Email").fill(username);
+        await page.getByLabel("Heslo").fill(password);
+        await page.getByRole("button", { name: "Přihlásit"}).click();
+
+        const currentUser = page
+            .locator(".navbar-right")
+            .locator("strong");
+        await expect(currentUser).toHaveText(userFullName);
+
+        await currentUser.click();
+        await page.locator("#logout-link").click();
+        await expect(currentUser).toBeVisible({ visible: false });
+        await expect(page.locator('.navbar-right')).toHaveText('Přihlásit');
+    });
+});
     
-    //Log in and get to applications page
-    await emailField.fill(username);
-    await passwordField.fill(password);
-    await loginButton.click();
-    await page.getByRole("link", {name: "Přihlášky"}).click();
-    await page.waitForLoadState();
-    await page.getByText('Provádím...').waitFor( { state: "hidden" });
+test.describe("Applications Page", () => {
+    test.beforeEach( async({ page }) => { 
+        await page.goto("/prihlaseni");
 
-    // Verify the number of application is correct
-    const tableSizeInfo = page.locator("#DataTables_Table_0_info");
-    await expect(tableSizeInfo).toHaveText(/Zobrazeno \d+ až \d+ záznamů z \d+/);
-    console.log(await tableSizeInfo.textContent());
+        await test.step("fill the valid user's email in the email field", async() => {
+            await page.getByLabel("Email").fill(username);
+        });
 
-    // Print all applications
-    const rows = await page
-        .locator("#DataTables_Table_0")
+        await test.step("fill the valid password in the password field", async() => {
+            await page.getByLabel("Heslo").fill(password);
+        });
+
+        await test.step("click on login button", async() => {
+            await page.getByRole("button", { name: "Přihlásit"}).click();
+        });
+
+        await page.getByRole("link", {name: "Přihlášky"}).click();
+        await page.waitForLoadState();
+
+        const loadingIndicator = page.locator("#DataTables_Table_0_processing");
+        await loadingIndicator.waitFor({state: "visible"});
+        await loadingIndicator.waitFor({state: "hidden"});
+
+        const pageTitle = await page.getByRole("heading", {level: 1});
+        await expect(pageTitle, 'page title should be displayed').toHaveText("Přihlášky");
+    });
+
+    test("Should list all applications", async({ page }) => {
+        const rows = await page
+            .locator(".dataTable")
+            .locator("tbody")
+            .locator("tr")
+            .all();
+
+        await expect(rows.length, "table should have >= " + applicationsPageSize + " rows")
+        .toBeGreaterThanOrEqual(applicationsPageSize);
+    });
+
+    test("Applications should contain data in correct format", async({ page}) => {
+        const rows = await page
+            .locator(".dataTable")
+            .locator("tbody")
+            .locator("tr")
+            .all();
+
+        for (const row of rows) {
+            const cells = row.locator("td");            
+            await expect(await cells.nth(0).textContent()).toMatch(RegExp.NAME);
+            await expect(await cells.nth(1).textContent()).toMatch(RegExp.DATE);
+            await expect(await cells.nth(2).textContent()).toMatch(RegExp.PAYMENT_TYPE);
+            await expect(await cells.nth(3).textContent()).toMatch(RegExp.TO_PAY);
+        }
+    });
+
+    test("Applications should be filtered", {
+        annotation: {
+            type: "Filtering scope",
+            description: "Applications are filtered also based on the information within the application details, not only from applications overview."
+        }
+    }, async ({ page }) => {
+        const rows = await page
+        .locator(".dataTable")
         .locator("tbody")
         .locator("tr")
         .all();
 
-    await expect(rows).toHaveLength(30);
-});
+        await page.locator("input[type='search']").fill(applicationsSearchText);
+        await page.waitForLoadState()
 
-test("Each application in the table contains all the data", async ({ page }) => {
-    await page.goto("/prihlaseni");
+        const loadingIndicator = page.getByText('Provádím...'); // page.locator('#DataTables_Table_0_processing');
+        await loadingIndicator.waitFor({state: "visible"});
+        await loadingIndicator.waitFor({state: "hidden"});
 
-    const emailField = page.getByLabel("Email");
-    const passwordField = page.getByLabel("Heslo");
-    const loginButton = page.getByRole("button", { name: "Přihlásit" });
-    
-    //Log in and get to applications page
-    await emailField.fill(username);
-    await passwordField.fill(password);
-    await loginButton.click();
-    await page.getByRole("link", {name: "Přihlášky"}).click();
-    await page.waitForLoadState();
-    await page.getByText('Provádím...').waitFor( { state: "hidden" });
+        const filteredRows = await page
+            .locator(".dataTable")
+            .locator("tbody")
+            .locator("tr")
+            .all();
 
-    const rows = await page
-    .locator("#DataTables_Table_0")
-    .locator("tbody")
-    .locator("tr")
-    .all();
+        await expect(filteredRows.length, "table should have < " + rows.length + " rows")
+        .toBeLessThan(rows.length);
 
-    for (const row of rows) {
-        const rowText = await row.textContent();
-        console.log(rowText);
-        const cells = row.locator("td");
-
-        await expect(cells.nth(0)).toHaveText(RegExp.NAME); // Each application contains name of participant
-        await expect(cells.nth(1)).toHaveText(RegExp.DATE); // Each application contains course term
-        await expect(cells.nth(2)).toHaveText(RegExp.PAYMENT_TYPE); // Each application contains payment type
-        await expect(cells.nth(3)).toHaveText(RegExp.TO_PAY); // Each application contains remaining price to pay
-    }
-});
-
-/* Cvičení 3 - Optional:
-    Vyplň něco do políčka "Hledat" a ověř, že se tabulka správně profiltrovala.
-*/
-test("Applications search is filtered correctly", async ({ page }) => {
-    await page.goto("/prihlaseni");
-
-    const emailField = page.getByLabel("Email");
-    const passwordField = page.getByLabel("Heslo");
-    const loginButton = page.getByRole("button", { name: "Přihlásit" });
-    
-    //Log in and get to applications page
-    await emailField.fill(username);
-    await passwordField.fill(password);
-    await loginButton.click();
-    await page.getByRole("link", {name: "Přihlášky"}).click();
-    await page.waitForLoadState();
-    await page.getByText('Provádím...').waitFor( { state: "hidden" });
-
-    const rows = await page
-        .locator("#DataTables_Table_0")
-        .locator("tbody")
-        .locator("tr")
-        .all();
-
-    // Fill something in the search field and verify the tab has been filtered correctly
-    await page.getByLabel('Hledat:').fill(applicationsSearchText);
-    await page.waitForLoadState();
-    await page.getByText('Provádím...').waitFor( { state: "hidden" });
-
-    const filteredRows = await page
-        .locator("#DataTables_Table_0")
-        .locator("tbody")
-        .locator("tr")
-        .all();
-
-    // Verifying the amount of filtered applications is less than the amount of all the applications.
-    console.log("After filtering there are " + filteredRows.length + " rows.");
-    console.log(filteredRows.length + " filtered rows should be less than " + rows.length + " rows.");
-    await expect(filteredRows.length).toBeLessThan(rows.length);
-
-    // Verifying the filtered applications include all the data (but would be better to have a separate test for this)
-    for (const row of filteredRows) {
-        await expect(row).toContainText(applicationsSearchText);
-        
-        const cells = row.locator("td");
-        await expect(await cells.nth(0).textContent()).toMatch(RegExp.NAME);
-        await expect(await cells.nth(1).textContent()).toMatch(RegExp.DATE);
-        await expect(await cells.nth(2).textContent()).toMatch(RegExp.PAYMENT_TYPE);
-        await expect(await cells.nth(3).textContent()).toMatch(RegExp.TO_PAY);
-    }
+        for (const row of filteredRows) {
+            const cells = row.locator("td");
+            await expect(await cells.nth(0).textContent()).toMatch(RegExp.NAME);
+            await expect(await cells.nth(1).textContent()).toMatch(RegExp.DATE);
+            await expect(await cells.nth(2).textContent()).toMatch(RegExp.PAYMENT_TYPE);
+            await expect(await cells.nth(3).textContent()).toMatch(RegExp.TO_PAY);
+        }
+    });
 });
